d1c93f3d7d4db216a7ae4be04c816870c931cde2
beomjunkim-soul <kbg142337@naver.com>
2025-07-21 12:10:13 +0900

    feat: MyBatis

---
+++ spring15-MyBatis-myproducts/src/main/java/org/kosa/myproject/Spring15MyBatisMyproductsApplication.java
@@ -0,0 +1,13 @@
+package org.kosa.myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring15MyBatisMyproductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring15MyBatisMyproductsApplication.class, args);
+	}
+
+}
