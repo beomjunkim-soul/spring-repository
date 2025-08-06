00a6c5a9a392f5a9a9b119e0cb8933281bd5a34f
beomjunkim-soul <kbg142337@naver.com>
2025-08-06 10:29:33 +0900

    feat: sping20 - transaction account done

--- spring20-account-service-junit-transaction-ajax/src/main/java/org/kosa/myproject/mapper/CustomerMapper.java
+++ spring20-account-service-junit-transaction-ajax/src/main/java/org/kosa/myproject/mapper/CustomerMapper.java
@@ -2,12 +2,13 @@ package org.kosa.myproject.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.kosa.myproject.domain.Customer;
-
 @Mapper
 public interface CustomerMapper {
-	public int getTotalCount();
 
-	public int register(Customer customer);
+	int register(Customer customer);
 
-	public Customer findById(Long customerId);
+	Customer findById(Long customerId);
+
+	Integer getTotalCount();
+
 }
